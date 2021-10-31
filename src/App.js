83f39b42c86dddfaaed045186d5fd73f9037b6d9
Angela <diegojuan2104@gmail.com>
2021-10-31 16:50:54 -0500

    Se agregaron las tablas de hg y flexa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 
 import * as React from 'react'
 
+//importar los datos 
+import * as departamentos from './datos/departamentos.json';
+import * as municipios from './datos/municipios.json';
+
+
 function App() {
+  //Definicion de los estados 
+  const [departamento, setDepartamento] = React.useState('') 
+  const [municipio, setMunicipio] = React.useState('') 
+
   return (
     <div className="App">
     </div>
